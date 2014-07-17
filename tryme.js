2da7ea8b998b6b050af5a77c587dc167df455568
uniquey <uniquey_1987@163.com>
2014-07-17 18:00:29 +0800

    second commit

--- tryme.js
+++ tryme.js
@@ -1,19 +1,22 @@
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to tryme.";
+  Template.names.vistornames = ["jack","joce","kit"];
+  Template.names.myname = "Yuki Lip";
+  Template.names.date = "2014.7.16";
+
+  Template.staff.person = [
+    {firstname:"jack",sex:"male"},
+    {firstname:"kit",sex:"female"},
+    {firstname:"alice",sex:"female"},
+    {firstname:"lei",sex:"male"}
+  ];
+  Template.girl.executive = function () {
+    return !!this.sex.match(/^m.*e$/)
   };
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
+  Template.nowdate.newdate = function () {
+    return new Date
+    
+  }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+};  
